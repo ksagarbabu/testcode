1905468068b33df8bc64d7031dbe015e5f294085
ksagarbabu <61538537+ksagarbabu@users.noreply.github.com>
2022-11-21 15:40:10 +0530

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,11 @@
+def fun(n):
+    e = n//3
+    q = n%3
+    if n == 0:
+        return '0'
+    elif e == 0:
+        return str(q)
+    else:
+        return ternary(e) + str(q)
+a=int(input()
+print(fun(a))
